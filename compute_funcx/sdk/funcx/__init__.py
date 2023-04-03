135dc84dc847cf0a6a8400304e339c5f872971ae
Lei Wang <lei@globus.org>
2023-04-03 13:37:28 -0400

    placeholder for import warning

--- compute_funcx/sdk/funcx/__init__.py
+++ compute_funcx/sdk/funcx/__init__.py
@@ -1,6 +1,8 @@
 """ Globus Compute, formerly funcX: Fast function serving for clouds,
  clusters and supercomputers.
 """
+import warnings
+
 from funcx.version import __version__ as _version
 
 __author__ = "The Globus Compute team"
@@ -8,5 +10,7 @@ __version__ = _version
 
 from globus_compute_sdk import Client as FuncXClient
 from globus_compute_sdk import Executor as FuncXExecutor
+
+warnings.warn("This package is deprecated XXX TBD")
 
 __all__ = ("FuncXExecutor", "FuncXClient")
